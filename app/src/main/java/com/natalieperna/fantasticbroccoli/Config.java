306eab391f6ef850e2ab75a999f812943eeac77e
Natalie Perna <nmperna@gmail.com>
2016-08-10 21:28:12 -0400

    No need to ignore Config for now, nothing secret yet.

---
+++ app/src/main/java/com/natalieperna/fantasticbroccoli/Config.java
@@ -0,0 +1,23 @@
+package com.natalieperna.fantasticbroccoli;
+
+public class Config {
+    public class ApiUrl {
+        private static final String BASE = "http://fantastic-broccoli.veyqhi8wir.us-west-2.elasticbeanstalk.com/api/";
+
+        public static final String LOGIN = BASE + "login";
+    }
+
+    public class SharedPref {
+        // Name of our shared preferences
+        public static final String STORE = "fantasticbroccoli";
+
+        // Key to store the boolean to track if user is logged in or not
+        public static final String LOGGEDIN = "loggedin";
+
+        // Key to store the API token of logged in user
+        public static final String TOKEN = "token";
+
+        // Key to store email of current logged in user
+        public static final String EMAIL = "email";
+    }
+}
