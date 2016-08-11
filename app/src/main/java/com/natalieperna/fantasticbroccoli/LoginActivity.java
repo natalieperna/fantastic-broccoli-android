53b1c4c22a0a0bca5cd5a871453a19b638a16b38
Natalie Perna <nmperna@gmail.com>
2016-08-10 21:18:02 -0400

    Remove dummy credentials and registration placeholder.

--- app/src/main/java/com/natalieperna/fantasticbroccoli/LoginActivity.java
+++ app/src/main/java/com/natalieperna/fantasticbroccoli/LoginActivity.java
@@ -56,13 +56,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
@@ -344,15 +337,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             RequestQueue queue = Volley.newRequestQueue(context);
             queue.add(jr);
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
             return true;
         }
 
